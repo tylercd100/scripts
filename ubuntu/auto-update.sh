b30ea31c2033c9736c53414c34f8f8420cc11ded
Tyler Arbon <tylercd100@gmail.com>
2017-09-26 13:40:15 -0600

    Create auto-update.sh

---
+++ ubuntu/auto-update.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install -y apticron sendmail cron-apt unattended-upgrades
+sudo dpkg-reconfigure unattended-upgrades
+sudo dpkg-reconfigure apticron
+sudo sendmailconfig
